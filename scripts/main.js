22365a3f81c911a9469e7688cd116ca6f4649580
Levi9111 <captainlevi9111@gmail.com>
2024-12-06 21:55:47 +0600

    homepage doen

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,26 @@
 'use strict';
+const navbar = document.querySelector('.navbar');
+const logo = document.getElementById('nav-logo');
+
+function updateNavOnScroll(scrollY) {
+  if (scrollY > 350) applyFixedNavStyles();
+  else resetNavStyles();
+}
+
+function applyFixedNavStyles() {
+  navbar.style.position = 'fixed';
+  logo.setAttribute('width', '105px');
+  logo.setAttribute('height', '108px');
+  navbar.style.background = '#050D04';
+}
+
+function resetNavStyles() {
+  navbar.style.position = 'absolute';
+  logo.setAttribute('width', '229px');
+  logo.setAttribute('height', '234px');
+  navbar.style.background = 'transparent';
+}
+window.addEventListener('scroll', function () {
+  const scrollY = window.scrollY;
+  updateNavOnScroll(scrollY);
+});
