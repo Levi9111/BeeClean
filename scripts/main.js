9ef82067dcb84591ffaf48fc65c6df3e0187b73b
Levi9111 <captainlevi9111@gmail.com>
2024-12-07 22:19:58 +0600

    responsive layput created

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,11 @@
 'use strict';
 const navbar = document.querySelector('.navbar');
+const responsiveNav = document.querySelector('.responsive-navbar');
+const responsiveNavbar = document.querySelector('.responsive-navbar');
+const resposniveNavUl = document.querySelector('.responsive-nav-items ul');
+const burgerMenu = document.querySelector('.burger-menu');
+const navItems = document.querySelector('.responsive-nav-items');
+
 const logo = document.getElementById('nav-logo');
 
 function updateNavOnScroll(scrollY) {
@@ -24,3 +30,57 @@ window.addEventListener('scroll', function () {
   const scrollY = window.scrollY;
   updateNavOnScroll(scrollY);
 });
+
+// Responsive nav styles
+
+let showNavList = false;
+
+function updateResNavScroll(scrollY) {
+  if (scrollY > 300) {
+    applyResNavStyles();
+  } else {
+    resetResNavStyles();
+  }
+}
+
+window.addEventListener('scroll', function () {
+  const scrollY = window.scrollY;
+  updateResNavScroll(scrollY);
+});
+
+function applyResNavStyles() {
+  responsiveNav.style.position = 'fixed';
+  responsiveNav.style.background = '#050D04';
+}
+
+function resetResNavStyles() {
+  responsiveNav.style.position = 'absolute';
+  responsiveNav.style.background = 'transparent';
+}
+
+function navListController() {
+  if (showNavList) {
+    navItems.style.top = '60px';
+    responsiveNav.style.background = '#050D04';
+  } else {
+    navItems.style.top = '-300px';
+  }
+}
+
+navListController();
+burgerMenu.addEventListener('click', function () {
+  showNavList = !showNavList;
+  navListController();
+});
+
+// @media only screen and (max-width: 600px) {
+// }
+
+// @media only screen and (max-width: 768px) {
+// }
+
+// @media only screen and (max-width: 992px) {
+// }
+
+// @media only screen and (max-width: 1200px) {
+// }
