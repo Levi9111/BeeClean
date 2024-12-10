d3602b31b5caef7f909674cfb8b1a9a3714e04ae
Levi9111 <captainlevi9111@gmail.com>
2024-12-10 13:20:42 +0600

    faq initiated

--- scripts/main.js
+++ scripts/main.js
@@ -5,26 +5,30 @@ const responsiveNavbar = document.querySelector('.responsive-navbar');
 const resposniveNavUl = document.querySelector('.responsive-nav-items ul');
 const burgerMenu = document.querySelector('.burger-menu');
 const navItems = document.querySelector('.responsive-nav-items');
-
 const logo = document.getElementById('nav-logo');
+const accordionLabels = document.querySelectorAll('.accordion label');
 
 function updateNavOnScroll(scrollY) {
-  if (scrollY > 350) applyFixedNavStyles();
+  if (scrollY > 250) applyFixedNavStyles();
   else resetNavStyles();
 }
 
 function applyFixedNavStyles() {
-  navbar.style.position = 'fixed';
-  logo.setAttribute('width', '105px');
-  logo.setAttribute('height', '108px');
-  navbar.style.background = '#050D04';
+  if (navbar && logo) {
+    navbar.style.position = 'fixed';
+    logo.setAttribute('width', '105px');
+    logo.setAttribute('height', '108px');
+    navbar.style.background = '#050D04';
+  }
 }
 
 function resetNavStyles() {
-  navbar.style.position = 'absolute';
-  logo.setAttribute('width', '229px');
-  logo.setAttribute('height', '234px');
-  navbar.style.background = 'transparent';
+  if (navbar && logo) {
+    navbar.style.position = 'absolute';
+    logo.setAttribute('width', '229px');
+    logo.setAttribute('height', '234px');
+    navbar.style.background = 'transparent';
+  }
 }
 window.addEventListener('scroll', function () {
   const scrollY = window.scrollY;
@@ -71,6 +75,24 @@ navListController();
 burgerMenu.addEventListener('click', function () {
   showNavList = !showNavList;
   navListController();
+});
+
+accordionLabels.forEach((label) => {
+  const labelImg = label.querySelector('span img');
+  label.addEventListener('click', function () {
+    accordionLabels.forEach((lbl) => {
+      const lblImg = lbl.querySelector('span img');
+      lbl.style.background = '#14213D1A';
+      lbl.style.color = '#14213D';
+      lblImg.style.filter = 'none';
+      lblImg.style.transform = 'rotate(360deg)';
+    });
+
+    label.style.background = '#14213D';
+    label.style.color = '#ffffff';
+    labelImg.style.filter = 'brightness(0) invert(1)';
+    labelImg.style.transform = 'rotate(180deg)';
+  });
 });
 
 // @media only screen and (max-width: 600px) {
